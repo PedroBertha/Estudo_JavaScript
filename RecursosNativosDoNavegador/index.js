67b437c6a8b7a66a370df1d3fa67d8664744bd3d
PedroBertha <phlikoskibertha@gmail.com>
2025-03-17 11:14:54 -0300

    estudando recursos nativos do navegador

---
+++ RecursosNativosDoNavegador/index.js
@@ -0,0 +1,11 @@
+let nome = "Pedro";
+
+console.log(nome); // Pedro
+console.error(nome);
+console.warn(nome);
+
+// window.alert(nome);
+// nome = window.prompt("Digite seu nome: ");
+// let confirmacao = window.confirm("Deseja confirmar essa ação?");
+
+ 
