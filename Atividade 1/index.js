916fd5732cef6c4e95b294094a484fcbea37b3fb
PedroBertha <phlikoskibertha@gmail.com>
2025-03-17 14:58:31 -0300

    primeiro exercício em JavaScript

---
+++ Atividade 1/index.js
@@ -0,0 +1,10 @@
+const primeiroNome = prompt("Indorme o primerio nome do recruta:");
+const sobrenome = prompt("Infirme o ultimo nome do recruta:");
+const campoDeEstudo = prompt("Informe o campo de estrudo:");
+const anoDeNascimento = prompt("Informe o ano de nascimento do recruta:");  
+
+alert (
+    "Recruta cadastrado com sucesso! \n" +
+    "\n Nome completo: " + primeiroNome + " " + sobrenome +
+    "\n Idade: " + (2025 - anoDeNascimento)
+)
