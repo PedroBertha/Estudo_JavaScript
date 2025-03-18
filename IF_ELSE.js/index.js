cd45ce3e6c0844ab6ef7f6af82f65ad6b4d64db3
PedroBertha <phlikoskibertha@gmail.com>
2025-03-18 20:13:01 -0300

    intrudução IF e ELSE

---
+++ IF_ELSE.js/index.js
@@ -0,0 +1,12 @@
+const idade = prompt ("Informe a sua idade: ")
+
+if (idade => 18) {
+   alert("Gandalf espera por você.")
+} else if (idade < 12) {
+    alert("Hoobit.")
+} 
+
+const resultado  = (6 === 6) ? "Verdadeiro" : "Falso"
+
+console.log(resultado)
+12
