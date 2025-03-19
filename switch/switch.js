27315632f591e8fe30fbd94442566caa3c8f6efd
PedroBertha <phlikoskibertha@gmail.com>
2025-03-18 21:29:40 -0300

    estudando switch

---
+++ switch/switch.js
@@ -0,0 +1,15 @@
+const reasultado = prompt("ERscolha uma alternativa: \na)\nb)\nc)")
+
+switch (reasultado) {
+    case "a":
+        alert("O reasultado é 'a'")
+        break
+    case "b":
+        alert("O reasultado é 'b'")
+        break
+    case "c":
+        alert("O reasultado é 'c'")
+        break
+    default:
+        alert("Finalizando")
+}
