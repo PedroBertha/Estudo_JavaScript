bb4ca22561ee61973cbb9ba551f5c649e8fe56e3
PedroBertha <phlikoskibertha@gmail.com>
2025-04-01 21:49:42 -0300

    estudando Obejeto

---
+++ JavaScript_II/objetos/index.js
@@ -0,0 +1,15 @@
+//É uma estrutura do JavaScript para se trabalhar com dicionários chave-valor
+
+//O par chave-valor em um objeto JavaScript é chamado de propriedade
+
+//Diferente dos arrays, seus elementos não possuem nenhuma sequência
+
+//Uma propriedade também pode ter strings, números e booleanos como chave
+
+//Uma propriedade pode armazenar qualquer tipo de dado como valor
+
+//Pode ser criado através de chaves {} → let objeto = {}
+
+//Suas propriedades e funções podem ser referenciadas
+//por encadeamento com ponto "." ou colchetes []
+
