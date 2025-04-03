598b202c94208ef8bd11367ca60d1277dc6513bf
PedroBertha <phlikoskibertha@gmail.com>
2025-04-02 22:05:01 -0300

    introdução fuction

---
+++ JavaScript_II/funções/introdução/index.js
@@ -0,0 +1,27 @@
+// Funções:
+
+// É uma estrutura do JavaScript que permite declarar um procedimento 
+// e executá-lo programaticamente.
+
+// Uma função pode ser vista como:  
+// 🔴 — ENTRADA → 🔴 — PROCEDIMENTO → 🔴 — SAÍDA  
+
+// Tanto a entrada quanto a saída de uma função são opcionais, 
+// ou seja, uma função pode existir sem um deles ou sem ambos.  
+
+// As entradas da função são chamadas de parâmetros.  
+// As saídas da função são chamadas de retorno.  
+
+// Sintaxe:
+// function nomeDaFuncao(entradas) {  
+    // procedimentos...  
+//  return saida;  
+// }
+
+function ola() {
+    console.log("Olá Mundo!")
+}
+
+ola()
+ola()
+ola()
